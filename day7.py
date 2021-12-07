0410510c42f5bac131e0ac855bac2d4aed167cd4
Alex Petrov <alex.petrov.vt@gmail.com>
2021-12-07 18:49:41 -0500

    Format files

--- day7.py
+++ day7.py
@@ -1,17 +1,19 @@
 from shared import get_lines_from_file
 
+
 def select_horizontal_position(positions):
     median = find_median(positions)
     fuel_cost = 0
     for position in positions:
-        fuel_cost += abs(position-median)
+        fuel_cost += abs(position - median)
     return fuel_cost
+
 
 def select_horizontal_position_part_2(positions):
     min_pos, max_pos = min(positions), max(positions)
 
-    min_fuel_cost = float('inf')
-    for i in range(min_pos, max_pos+1):
+    min_fuel_cost = float("inf")
+    for i in range(min_pos, max_pos + 1):
         curr_fuel_cost = 0
         for position in positions:
             curr_fuel_cost += calculate_move_fuel_cost(position, i)
@@ -19,26 +21,27 @@ def select_horizontal_position_part_2(positions):
 
     return int(min_fuel_cost)
 
+
 def calculate_move_fuel_cost(pos1, pos2):
-    distance = abs(pos1-pos2)
-    return distance*(distance+1)/2
+    distance = abs(pos1 - pos2)
+    return distance * (distance + 1) / 2
+
 
 def find_median(positions):
     sorted_positions = sorted(positions)
-    if len(sorted_positions)%2 == 1:
-        return sorted_positions[int(len(sorted_positions)/2)]
+    if len(sorted_positions) % 2 == 1:
+        return sorted_positions[int(len(sorted_positions) / 2)]
     else:
-        middle = int(len(sorted_positions)/2)
-        return int((sorted_positions[middle]+sorted_positions[middle-1])/2)
+        middle = int(len(sorted_positions) / 2)
+        return int((sorted_positions[middle] + sorted_positions[middle - 1]) / 2)
+
 
 if __name__ == "__main__":
     lines = get_lines_from_file("./input/day7.txt")
     assert len(lines) == 1
 
-    positions_as_str = lines[0].strip().split(',')
+    positions_as_str = lines[0].strip().split(",")
     positions = list(map(int, positions_as_str))
 
     print(select_horizontal_position(positions))
     print(select_horizontal_position_part_2(positions))
-
-
--- test_day7.py
+++ test_day7.py
@@ -1,13 +1,19 @@
-from day7 import select_horizontal_position, select_horizontal_position_part_2, find_median
+from day7 import (
+    select_horizontal_position,
+    select_horizontal_position_part_2,
+    find_median,
+)
+
 
 def test_selecting_horizontal_position_part_1():
-    positions = [16,1,2,0,4,2,7,1,2,14]
+    positions = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
     print(sorted(positions))
     print(find_median(positions))
     min_fuel = select_horizontal_position(positions)
     assert min_fuel == 37
 
+
 def test_selecting_horizontal_position_part_2():
-    positions = [16,1,2,0,4,2,7,1,2,14]
+    positions = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
     min_fuel = select_horizontal_position_part_2(positions)
     assert min_fuel == 168
