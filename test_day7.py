cf41be1bee6dd753a0c5b2d8b79f463c2616129b
Alex Petrov <apetrov@connected2fiber.com>
2021-12-08 20:20:11 -0500

    Remove unused import

--- test_day7.py
+++ test_day7.py
@@ -1,14 +1,11 @@
 from day7 import (
     select_horizontal_position,
-    select_horizontal_position_part_2,
-    find_median,
+    select_horizontal_position_part_2
 )
 
 
 def test_selecting_horizontal_position_part_1():
     positions = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
-    print(sorted(positions))
-    print(find_median(positions))
     min_fuel = select_horizontal_position(positions)
     assert min_fuel == 37
 
